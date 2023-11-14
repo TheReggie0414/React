d5023196b71e7d51347758301ab5a02795507194
Vladyslav Kysil <109174554+TheReggie0414@users.noreply.github.com>
2023-11-14 17:28:41 +0200

    Delete src directory

--- src/App.jsx
+++
@@ -1,8 +0,0 @@
-import React, { useState } from "react";
-import "./App.css";
-export const App = () => {
-    const result = div(4, 2);
-    const sum = add(2, 2);
-
-    useState();
-};
--- src/sections/mainIndex/mainIndex.jsx
+++
@@ -1,7 +0,0 @@
-import "./mainIndex.css";
-/* import { ButtonComponent } from "../../components/Button"; */
-export const mainIndex = () => {
-    return (
-        <h2>ТУТ</h2>
-    );
-};
